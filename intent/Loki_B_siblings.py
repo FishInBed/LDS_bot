1a59b875f1bc74a7428e500e533e261ed683a221
FishInBed <c5227c@gmail.com>
2023-08-15 09:46:53 +0800

    add part A part B intents

---
+++ intent/Loki_B_siblings.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+"""
+    Loki module for B_siblings
+
+    Input:
+        inputSTR      str,
+        utterance     str,
+        args          str[],
+        resultDICT    dict,
+        refDICT       dict
+
+    Output:
+        resultDICT    dict
+"""
+
+from random import sample
+import json
+import os
+
+DEBUG_B_siblings = True
+CHATBOT_MODE = True
+
+userDefinedDICT = {}
+try:
+    userDefinedDICT = json.load(open(os.path.join(os.path.dirname(__file__), "USER_DEFINED.json"), encoding="utf-8"))
+except Exception as e:
+    print("[ERROR] userDefinedDICT => {}".format(str(e)))
+
+responseDICT = {}
+if CHATBOT_MODE:
+    try:
+        responseDICT = json.load(open(os.path.join(os.path.dirname(os.path.dirname(__file__)), "reply/reply_B_siblings.json"), encoding="utf-8"))
+    except Exception as e:
+        print("[ERROR] responseDICT => {}".format(str(e)))
+
+# 將符合句型的參數列表印出。這是 debug 或是開發用的。
+def debugInfo(inputSTR, utterance):
+    if DEBUG_B_siblings:
+        print("[B_siblings] {} ===> {}".format(inputSTR, utterance))
+
+def getResponse(utterance, args):
+    resultSTR = ""
+    if utterance in responseDICT:
+        if len(responseDICT[utterance]):
+            resultSTR = sample(responseDICT[utterance], 1)[0].format(*args)
+
+    return resultSTR
+
+def getResult(inputSTR, utterance, args, resultDICT, refDICT):
+    debugInfo(inputSTR, utterance)
+    if utterance == "[1個][哥哥][1個][姐姐]":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "[1個][哥哥][1個][姐姐][1個][妹妹][1個][弟弟]":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "[1個][哥哥][1個][姐姐][1個][妹妹]跟[1個][弟弟]":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "[1個][哥哥][1個][姐姐][1個][弟弟]":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "[1個][哥哥][1個][姐姐]跟[1個][弟弟]":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "[哥哥][姐姐]各[1個]":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "[哥哥]跟[姐姐]各[1個]":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "有":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "有[1個][哥哥]":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "有[1個][哥哥]跟[1個][姐姐]":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "沒有":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    return resultDICT
---
+++ intent/Loki_age.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+"""
+    Loki module for age
+
+    Input:
+        inputSTR      str,
+        utterance     str,
+        args          str[],
+        resultDICT    dict,
+        refDICT       dict
+
+    Output:
+        resultDICT    dict
+"""
+
+from random import sample
+import json
+import os
+
+DEBUG_age = True
+CHATBOT_MODE = True
+
+userDefinedDICT = {}
+try:
+    userDefinedDICT = json.load(open(os.path.join(os.path.dirname(__file__), "USER_DEFINED.json"), encoding="utf-8"))
+except Exception as e:
+    print("[ERROR] userDefinedDICT => {}".format(str(e)))
+
+responseDICT = {}
+if CHATBOT_MODE:
+    try:
+        responseDICT = json.load(open(os.path.join(os.path.dirname(os.path.dirname(__file__)), "reply/reply_age.json"), encoding="utf-8"))
+    except Exception as e:
+        print("[ERROR] responseDICT => {}".format(str(e)))
+
+# 將符合句型的參數列表印出。這是 debug 或是開發用的。
+def debugInfo(inputSTR, utterance):
+    if DEBUG_age:
+        print("[age] {} ===> {}".format(inputSTR, utterance))
+
+def getResponse(utterance, args):
+    resultSTR = ""
+    if utterance in responseDICT:
+        if len(responseDICT[utterance]):
+            resultSTR = sample(responseDICT[utterance], 1)[0].format(*args)
+
+    return resultSTR
+
+def getResult(inputSTR, utterance, args, resultDICT, refDICT):
+    debugInfo(inputSTR, utterance)
+    if utterance == "[他][1]歲":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "[他][1]歲[2]個月":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "[他][2]個月[大]":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    return resultDICT
