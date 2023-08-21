2f01f9670e21ff115c6ce640f2cffb73909255c9
In Bed <31921056+FishInBed@users.noreply.github.com>
2023-08-21 13:20:30 +0800

    Add revised context: environment

---
+++ B_environment/intent/Loki_3C.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+"""
+    Loki module for 3C
+
+    Input:
+        inputSTR      str,
+        utterance     str,
+        args          str[],
+        resultDICT    dict,
+        refDICT       dict
+
+    Output:
+        resultDICT    dict
+"""
+
+from random import sample
+import json
+import os
+
+DEBUG_3C = True
+CHATBOT_MODE = False
+
+userDefinedDICT = {}
+try:
+    userDefinedDICT = json.load(open(os.path.join(os.path.dirname(__file__), "USER_DEFINED.json"), encoding="utf-8"))
+except Exception as e:
+    print("[ERROR] userDefinedDICT => {}".format(str(e)))
+
+responseDICT = {}
+if CHATBOT_MODE:
+    try:
+        responseDICT = json.load(open(os.path.join(os.path.dirname(os.path.dirname(__file__)), "reply/reply_3C.json"), encoding="utf-8"))
+    except Exception as e:
+        print("[ERROR] responseDICT => {}".format(str(e)))
+
+# 將符合句型的參數列表印出。這是 debug 或是開發用的。
+def debugInfo(inputSTR, utterance):
+    if DEBUG_3C:
+        print("[3C] {} ===> {}".format(inputSTR, utterance))
+
+def getResponse(utterance, args):
+    resultSTR = ""
+    if utterance in responseDICT:
+        if len(responseDICT[utterance]):
+            resultSTR = sample(responseDICT[utterance], 1)[0].format(*args)
+
+    return resultSTR
+
+def getResult(inputSTR, utterance, args, resultDICT, refDICT):
+    debugInfo(inputSTR, utterance)
+    if utterance == "一天大概30分鐘":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "每天大概使用平板電腦30分鐘。":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "每天用平板大概30分鐘":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    return resultDICT
