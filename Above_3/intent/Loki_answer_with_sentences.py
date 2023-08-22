853586f9ff730834e06d4f004932713e24d8ca48
Ruo-Chi <126684202+Ruo-Chi@users.noreply.github.com>
2023-08-22 23:28:37 +0800

    Add files via upload

---
+++ Above_3/intent/Loki_answer_with_sentences.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+"""
+    Loki module for answer_with_sentences
+
+    Input:
+        inputSTR      str,
+        utterance     str,
+        args          str[],
+        resultDICT    dict,
+        refDICT       dict
+
+    Output:
+        resultDICT    dict
+"""
+
+from random import sample
+import json
+import os
+
+DEBUG_answer_with_sentences = True
+CHATBOT_MODE = True
+
+userDefinedDICT = {}
+try:
+    userDefinedDICT = json.load(open(os.path.join(os.path.dirname(__file__), "USER_DEFINED.json"), encoding="utf-8"))
+except Exception as e:
+    print("[ERROR] userDefinedDICT => {}".format(str(e)))
+
+responseDICT = {}
+if CHATBOT_MODE:
+    try:
+        responseDICT = json.load(open(os.path.join(os.path.dirname(os.path.dirname(__file__)), "reply/reply_answer_with_sentences.json"), encoding="utf-8"))
+    except Exception as e:
+        print("[ERROR] responseDICT => {}".format(str(e)))
+
+# 將符合句型的參數列表印出。這是 debug 或是開發用的。
+def debugInfo(inputSTR, utterance):
+    if DEBUG_answer_with_sentences:
+        print("[answer_with_sentences] {} ===> {}".format(inputSTR, utterance))
+
+def getResponse(utterance, args):
+    resultSTR = ""
+    if utterance in responseDICT:
+        if len(responseDICT[utterance]):
+            resultSTR = sample(responseDICT[utterance], 1)[0].format(*args)
+
+    return resultSTR
+
+def getResult(inputSTR, utterance, args, resultDICT, refDICT):
+    debugInfo(inputSTR, utterance)
+    if utterance == "[不常]":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "[不行]":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "[好像][可以]":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "[好像]不[會]":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "[很常][這樣]":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "[會]":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "[會]但[不常]":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "[會]但不愛說":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "[會]但不愛講":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "[都][可以]":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "不[會]":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "不太[會]":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "不太理人":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "很少":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "看[心情]":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    return resultDICT
