ee8edd1f4a0e5cdc72548ccfa258797885a5bcd8
Ruo-Chi <126684202+Ruo-Chi@users.noreply.github.com>
2023-08-22 23:30:41 +0800

    Add files via upload

---
+++ Above_6/intent/Loki_describe_3_or_4_pictures.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+"""
+    Loki module for describe_3_or_4_pictures
+
+    Input:
+        inputSTR      str,
+        utterance     str,
+        args          str[],
+        resultDICT    dict,
+        refDICT       dict
+
+    Output:
+        resultDICT    dict
+"""
+
+from random import sample
+import json
+import os
+
+DEBUG_describe_3_or_4_pictures = True
+CHATBOT_MODE = True
+
+userDefinedDICT = {}
+try:
+    userDefinedDICT = json.load(open(os.path.join(os.path.dirname(__file__), "USER_DEFINED.json"), encoding="utf-8"))
+except Exception as e:
+    print("[ERROR] userDefinedDICT => {}".format(str(e)))
+
+responseDICT = {}
+if CHATBOT_MODE:
+    try:
+        responseDICT = json.load(open(os.path.join(os.path.dirname(os.path.dirname(__file__)), "reply/reply_describe_3_or_4_pictures.json"), encoding="utf-8"))
+    except Exception as e:
+        print("[ERROR] responseDICT => {}".format(str(e)))
+
+# 將符合句型的參數列表印出。這是 debug 或是開發用的。
+def debugInfo(inputSTR, utterance):
+    if DEBUG_describe_3_or_4_pictures:
+        print("[describe_3_or_4_pictures] {} ===> {}".format(inputSTR, utterance))
+
+def getResponse(utterance, args):
+    resultSTR = ""
+    if utterance in responseDICT:
+        if len(responseDICT[utterance]):
+            resultSTR = sample(responseDICT[utterance], 1)[0].format(*args)
+
+    return resultSTR
+
+def getResult(inputSTR, utterance, args, resultDICT, refDICT):
+    debugInfo(inputSTR, utterance)
+    if utterance == "[不常]":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "[不行]":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "[會]":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "[會]跳來跳去":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "不[會]":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "不太[會]":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "不太行":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "沒辦法":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "看[心情]":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    if utterance == "講不[完整]":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            # write your code here
+            pass
+
+    return resultDICT
